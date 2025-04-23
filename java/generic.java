c2220c4936294b2acfdf2c184785201ef0a1e0b1
tripathidivyansh <tripatidivyansh144@gmail.com>
2025-04-23 17:12:46 +0530

    generic in java

---
+++ java/generic.java
@@ -0,0 +1,26 @@
+
+class Generic<t>{
+    private t value;
+
+    public void setvalue(t value){
+        this.value = value;
+    }
+    public t getvalue(){
+        return value;
+    }
+}
+
+public class generic {
+    
+    public static void main(String[] args) {
+        Generic<Integer> obj = new Generic<>();
+
+        obj.setvalue( 07);
+        System.out.println(obj.getvalue());
+
+        Generic<String> obj1 = new  Generic<>();
+        obj1.setvalue("malik");
+        obj1.getvalue();
+
+    }
+}
